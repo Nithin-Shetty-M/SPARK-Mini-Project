57a3d37c572a4ac57992a31645694e89b5a1302b
Nithin-Shetty-M <mnithinshetty15@gmail.com>
2026-03-12 21:25:09 +0530

    Added ORM model database table creation code to app.py and also folder creation code

--- app.py
+++ app.py
@@ -10,6 +10,49 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 db = SQLAlchemy(app)
 
+# --- Database Models ---
+
+class Guide(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    department = db.Column(db.String(50))
+    is_active = db.Column(db.Boolean, default=True)
+    projects = db.relationship('Project', backref='guide', lazy=True)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    stored_name = db.Column(db.String(200))
+    submission_date = db.Column(db.DateTime, default=datetime.utcnow)
+    # student_reg_nos = db.Column(db.Text)
+    course = db.Column(db.String(50))
+    academic_year = db.Column(db.String(10))
+    batch = db.Column(db.String(20))
+    status = db.Column(db.String(20), default='Pending')
+    rejection_reason = db.Column(db.Text, nullable=True)
+    guide_id = db.Column(db.String(50), db.ForeignKey('guide.id'))
+    students= db.relationship('Student',backref='project',lazy=True)
+
+
+class Student(db.Model):
+    roll_no = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(100))
+    course = db.Column(db.String(20))
+    section= db.column(db.string(5))
+    academic_year= db.column(db.string(10))
+    project_id = db.Column(db.String(50), db.ForeignKey('project.id'))
+
+
+class Student_login(db.Model):
+    roll_no = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(100))
+    course = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    batch = db.Column(db.String(20))
+
 # --- Routes ---
 
 @app.route('/')
@@ -20,4 +63,8 @@ def index():
 
 # Main loop
 if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
